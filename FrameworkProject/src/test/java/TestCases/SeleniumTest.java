aa3a691984b7929869c20f66e056ede2f0e1c7d9
sagar <sagak50@gmail.com>
2020-02-26 20:59:42 +0530

    first commit

---
+++ FrameworkProject/src/test/java/TestCases/SeleniumTest.java
@@ -0,0 +1,20 @@
+package TestCases;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void test()
+	{
+		
+		System.out.println("Hello");
+	}
+	
+	@Test
+	public void test2()
+	{
+		
+		System.out.println("Hola");
+	}
+
+}
---
+++ FrameworkProject/src/test/java/TestCases/javaTest.java
@@ -0,0 +1,13 @@
+package TestCases;
+import org.testng.annotations.Test;
+public class javaTest {
+	
+		
+		@Test
+		public void test3()
+		{
+			
+			System.out.println("sayonara");
+		}
+
+}
